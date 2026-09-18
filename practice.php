88d3071d12ec35254dd4dc1de072450670851ee0
juweriaabdi-dev <mjuweriyeabdi28@gmail.com>
2026-09-18 19:48:27 +0300

    Add files via upload

---
+++ practice.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Eng juuabdi</title>
+</head>
+
+<body>
+
+    <h1>welcome juuabdi</h1>
+
+    <?php
+ 
+     echo "Hello World";
+
+
+     //EACHO WITH PARANTHESES
+
+   
+     echo("welcome to jamhuriya");
+       
+    ?>
+
+</body>
+</html>
---
+++ print.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=p, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Print is used to display output in PHP. 
+    // It can display text and variable values.
+   $Fullname = "JUWERIA ABDI";
+        print " name is -$Fullname"; 
+
+        
+    ?>
+</body>
+</html>
